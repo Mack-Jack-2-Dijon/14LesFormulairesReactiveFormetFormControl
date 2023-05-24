883290eccfc8412461641cb80f15b38f3cb4ea2d
THAMEE Jacques (SNCF RESEAU / EIC BOURGOGNE FRANCHE COMTE / GEVREY VP) <8611308t@commun.ad.sncf.fr>
2023-05-24 11:36:53 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '14LesFormulairesReactiveFormetFormControl';
+  username = new FormControl('');
+  email = new FormControl('');
+  password = new FormControl('');
+  street = new FormControl('');
+  zip = new FormControl('');
+  city = new FormControl('');
+  submit: boolean = false;
+  newUser!: any;
+
+  inscription() {
+    this.submit = true;
+
+    this.newUser = {
+      username: this.username.value,
+      email: this.email.value,
+      password: this.password.value,
+      adress: {
+        street: this.street.value,
+        zip: this.zip.value,
+        city: this.city.value,
+      },
+    };
+  }
+}
